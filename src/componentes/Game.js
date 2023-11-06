de78cc724acafcee98267ed1c1015984b8b1e3db
Arthur Roque <arthur_roqq@outlook.com>
2023-11-06 13:18:41 -0300

    css componente game

--- src/componentes/Game.js
+++ src/componentes/Game.js
@@ -1,7 +1,7 @@
 import "./Game.css";
 const Game = ({verifyLetter}) => {
   return (
-    <div className="game">
+  <div className="game">
       <p className="points">  
         <span> Pontuação: 000</span>
       </p>
@@ -9,20 +9,23 @@ const Game = ({verifyLetter}) => {
       <h3 className="tip"> 
         Dica sobre a palavra: <span>Dica... </span>
       </h3>
-     <div className="wordContainer">
+    <div className="wordContainer">
         <span className="letter"> A </span>  
         <span className="blankSquare"> </span>
-     </div>
+    </div>
     <div className="lettercontainer">
       <p> Tente adivinhar uma letra da palavra: </p>
       <form> 
-        <input type="text" name="letter" maxLength="1" required/> 
-        <button>Jogar!</button>  
+       <input type="text" name="letter" maxLength="1" required/> 
+       <button>Jogar!</button>  
       </form>
-
     </div>
-    
-    </div> 
+    <div className="wrongLettersContainer"> 
+        <p>Letras já utilizadas: </p>
+        <span> a,</span>
+        <span> b, </span> 
+    </div>
+  </div> 
   )
 }
 
