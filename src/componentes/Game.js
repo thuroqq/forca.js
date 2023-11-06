45e3215fd7ac1a45c4a08be4f2be54f25f406bdb
Arthur Roque <arthur_roqq@outlook.com>
2023-11-06 12:56:07 -0300

    criação d componente

--- src/componentes/Game.js
+++ src/componentes/Game.js
@@ -1,11 +1,28 @@
 import "./Game.css";
 const Game = ({verifyLetter}) => {
   return (
-    <div>
-      
-      <h1>Game</h1>
-      <button onClick={verifyLetter}> finalizar jogo</button>
-      </div> 
+    <div className="game">
+      <p className="points">  
+        <span> Pontuação: 000</span>
+      </p>
+      <h1> Adivinhe a palavra:</h1>
+      <h3 className="tip"> 
+        Dica sobre a palavra: <span>Dica... </span>
+      </h3>
+     <div className="wordContainer">
+        <span className="letter"> A </span>  
+        <span className="blankSquare"> </span>
+     </div>
+    <div className="lettercontainer">
+      <p> Tente adivinhar uma letra da palavra: </p>
+      <form> 
+        <input type="text" name="letter" maxLength="1" required/> 
+        <button>Jogar!</button>  
+      </form>
+
+    </div>
+    
+    </div> 
   )
 }
 
